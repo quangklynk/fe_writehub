2cb587cdfb57ff0cd554d4440e96fbf134a09c81
Quang Trương <quangklynk@gmail.com>
2022-01-10 16:31:05 +0700

    first cm

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Menu />
+  </div>
+</template>
+
+<script>
+import  Menu  from "./components/Auth/Index.vue";
+export default {
+  name: "App",
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Menu/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "./../Auth/Index.vue";
+export default {
+  name: "Menu",
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+</style>
